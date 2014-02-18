4967f408d37ab6b1943146fae52a893075bc4626
Christophe FEIJOO <cfeijoo@e2r13p7.42.fr>
2014-02-18 18:27:23 +0100

    Updated parser, now splits lines

--- parseFile.js
+++ parseFile.js
@@ -2,65 +2,83 @@
 **	File Parser
 */
 
-function	checkFunctions(name, data)
+function	checkFunction(name, data, i, fileInfo)
 {
 	// This Regex still doesn't match functions as parameters
-	// var functionsLocationRegex = /^([\t\s]+)([0-9a-zA-Z_]+ )?([0-9a-zA-Z_]+)([\t\s]+)(\**)([\t\s]+)?([0-9a-zA-Z_]+)\(((([0-9a-zA-Z_]+ )?([0-9a-zA-Z_]+)([\t\s]+)?(\**)([\t\s]+)?([0-9a-zA-Z_]+)(, ))*(([0-9a-zA-Z_]+ )?([0-9a-zA-Z_]+)([\t\s]+)?(\**)([\t\s]+)?([0-9a-zA-Z_]+)))?\)/mgi;
-	var	functionsLocationRegex = /^([\t\s]+)([0-9a-zA-Z_]+ )?([0-9a-zA-Z_]+)([\t\s]+)(\**)([\t\s]+)?([0-9a-zA-Z_]+)\(((([0-9a-zA-Z_]+ )?([0-9a-zA-Z_]+)([\t\s]+)?(\**)([\t\s]+)?([0-9a-zA-Z_]+)(, ))*(([0-9a-zA-Z_]+ )?([0-9a-zA-Z_]+)([\t\s]+)?(\**)([\t\s]+)?([0-9a-zA-Z_]+)))?\)\n{(\n[\s\t0-9a-zA-Z;=,.\->_\*\(\)+<>&)\[\]]*)*}/mgi;
+	var isFunctionRegex = /^([\t\s]*)([0-9a-zA-Z_]+ )?([0-9a-zA-Z_]+)([\t\s]+)(\**)([\t\s]+)?([0-9a-zA-Z_]+)\(((([0-9a-zA-Z_]+ )?([0-9a-zA-Z_]+)([\t\s]+)?(\**)([\t\s]+)?([0-9a-zA-Z_]+)(?:, ))*(([0-9a-zA-Z_]+ )?([0-9a-zA-Z_]+)([\t\s]+)?(\**)([\t\s]+)?([0-9a-zA-Z_]+)))?\)[\t\s]*(?!;)(?:{)?$/mgi;
 
-
-	// SHOULD CHECK THE FUNCTION CONTENT MNUALLY (WITHOUT REGEX)
-
-	var functionsLocation = data.match(functionsLocationRegex);
-
-	console.log(".........................");
-	console.log(functionsLocation);
-	if (!functionsLocation)
-		console.error(colors.error + "There is no function in the file." + colors.reset);
-	else if (functionsLocation.length > 5)
-		console.error(colors.error + functionsLocation.length + " functions in file!" + colors.reset);
+	var isFunction = data[i].match(isFunctionRegex);
+	// Should check umcompleteFunctionPrototypeRegex FIRST!
+	if (isFunction)
+	{
+		console.log(data[i]);
+		data[i].isFunctionStart = 1;
+		fileInfo.functionsCount++;
+	}
 	else
-		console.log(colors.positive + functionsLocation.length + " functions in file." + colors.reset);
+		data[i].isFunctionStart = 0;
 }
 
-function	checkTrailingWhitespaces()
+function	checkTrailingWhitespaces(data, i)
 {
 	var	trailingWhitespacesRegex = /[\t\s ]+$/mgi;
-	var	trailingWhitespaces = data.match(trailingWhitespacesRegex);
+	var	trailingWhitespaces = data[i].match(trailingWhitespacesRegex);
 
-	console.log(".........................");
-	console.log(trailingWhitespaces);
+	if (trailingWhitespaces)
+		console.log(colors.error + "Line " + i + ": Trailing whitespaces" + colors.reset)
 }
 
-function	checkEmptyLines()
+function	checkEmptyLines(data, i)
 {
-	var	emptyLinesRegex = /\n\n/mgi;
-	var	emptyLines = data.match(emptyLinesRegex);
+	var	emptyLineRegex = /^$/mgi;
+	var	emptyLine = data[i].match(emptyLineRegex);
 
 	// After this, check if this empty line is in a good place
-	console.log(".........................");
-	console.log(emptyLines);
+	if (emptyLine)
+	{
+		data[i].isEmptyLine = true;
+		if (i > 0 && data[i - 1].isEmptyLine)
+			console.log(colors.error + "Line " + (i - 1) + ": Multiple empty lines." + colors.reset)
+	}
 }
 
-function	checkEndFileReturn()
+function	checkEndFileReturn(data, i)
 {
-	var	endFileReturnRegex = /$\Z/mgi;
-	var	endFileReturn = data.match(endFileReturnRegex);
+	var	endFileReturnRegex = /^$/mgi;
+	var	endFileReturn = data[i].match(endFileReturnRegex);
 
-	console.log(".........................");
 	if (!endFileReturn)
 		console.error(colors.error + "Missing carriage return at end of file" + colors.reset);
 }
 
+function	displayFunctionsCount(fileInfo)
+{
+	if (!fileInfo.functionsCount)
+		console.error(colors.error + "There is no function in the file." + colors.reset);
+	else if (fileInfo.functionsCount > 5)
+		console.error(colors.error + fileInfo.functionsCount + " functions in file!" + colors.reset);
+	else
+		console.log(colors.positive + fileInfo.functionsCount + " functions in file." + colors.reset);
+}
+
+
 function	parseFile(name, data)
 {
+	// if (kubinette.visualMode)
 	console.log(colors.discrete + "File : " + name + colors.reset);
-	if (kubinette.visualMode)
-		console.log(data);
-	checkFunctions(name, data);
-	checkTrailingWhitespaces();
-	checkEndFileReturn();
-	checkEmptyLines();
+	var data = data.split('\n');
+	var	fileInfo = new Array();
+	fileInfo.functionsCount = 0;
+
+	for (var i in data)
+	{
+		if (i > 0 && !data[i - 1].isFunctionStart && !data[i - 1].isCompletePrototype)
+			checkFunction(name, data, i, fileInfo);
+		checkTrailingWhitespaces(data, i);
+		checkEmptyLines(data, i);
+	}
+	checkEndFileReturn(data, i);
+	displayFunctionsCount(fileInfo);
 }
 
 module.exports = parseFile;
